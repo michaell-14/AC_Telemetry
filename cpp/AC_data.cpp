bdd8c3436057eae1e5ce58300feb90269d378655
michaell-14 <121648960+michaell-14@users.noreply.github.com>
2026-04-02 12:11:47 -0700

    Create AC_data.cpp
    Created car data packet

---
+++ cpp/AC_data.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <complex>
+//#include <fftw3.h> // FFT library
+
+#pragma pack(push, 1) // Defiing structure of UDP data
+struct CarData {
+    char identifier; // identifier for structed data
+    int32_t size; // size of data packet in bytes
+
+    float speed_kmh; // speed in km/h
+    float speed_mph; // in mph
+    float speed_ms; // in m/s
+
+    //abs
+    int8_t isAbsEnabled; // ABS on or off
+    int8_t isAbsInAction; // ABS status
+
+    //tc
+    int8_t isTcEnabled; // Traction control on or off
+    int8_t isTcInAction; // Traction control status
+
+    //pit
+    int8_t isInPit;
+    int8_t isEngineLimiterOn;
+
+    //G force
+    float accG_vertical;
+    float accG_horizontal;
+    float accG_frontal;
+
+    //Timing
+    int32_t lapTime;
+    int32_t lastLap;
+    int32_t bestLap;
+    int32_t lapCount;
+
+    //Engine
+    float gas;
+    float brake;
+    float clutch;
+    float engineRPM;
+    float steer;
+    int32_t gear;
+    float cgHeight; // Height relative to centre of gravity
+
+    //Wheel
+    float wheelAngularSpeed[4]; // Angular speed of each wheel
+    float slipAngle[4]; // Slip angle of each wheel
+    float slipAngle_ContactPatch[4]; //slip angle on contact patch
+    float slipRatio[4]; //slip ratio of each wheel
+    float tyreSlip[4];
+    float ndSlip[4];
+    float load[4]; //load on each wheel
+    float Dy[4]; //lateral friction
+    float Mz[4]; //longitudinal friction
+    float tyreDirtyLevel[4];
+
+    float camberRAD[4]; //camber angle in radians
+    float tyreRadius[4]; 
+    float tyreLoadedRadius[4]; //radius of tyre under load
+
+    //suspension
+    float suspensionHeight[4]; //suspension height of each wheel
+    float carPositionNormalized;
+    float carSlope;
+    float carCoordinates[3]; // x, y, z coordinates of the car in the track reference
+};
+#pragma pack(pop)
